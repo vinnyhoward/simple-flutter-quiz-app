bf696c102dde29ddcd209046674e3b8ea4ca52c7
Vince Howard <vincenguyenhoward@gmail.com>
2020-07-28 23:02:35 -0600

    Added buttons and children widgets for questions

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,27 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(App());
-}
+void main() => runApp(App());
 
 class App extends StatelessWidget {
+  @override
   Widget build(BuildContext ctx) {
+    var quizQuestions = [
+      'What\'s your favorite anime',
+      'What\'s your favorite video game'
+    ];
     return MaterialApp(
-      home: Text('First Flutter App'),
+      home: Scaffold(
+          appBar: AppBar(
+            title: Text('My first App'),
+          ),
+          body: Column(
+            children: <Widget>[
+              Text('Questions'),
+              RaisedButton(child: Text('Asswer 1'), onPressed: null),
+              RaisedButton(child: Text('Asswer 2'), onPressed: null),
+              RaisedButton(child: Text('Asswer 3'), onPressed: null),
+            ],
+          )),
     );
   }
 }
