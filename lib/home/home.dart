05cf0b6c89eafcf598fe647da07bbc64f55ad9c8
Vince Howard <vincenguyenhoward@gmail.com>
2020-08-16 22:45:01 -0600

    adding in basic skeleton for home screen and side scrolling list for chosing which difficulty

--- lib/home/home.dart
+++ lib/home/home.dart
@@ -6,20 +6,55 @@ class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('First Route'),
-      ),
-      body: Center(
-        child: RaisedButton(
-          child: Text('Open route'),
-          onPressed: () {
-            Navigator.push(
-              context,
-              MaterialPageRoute(builder: (context) => Quiz()),
-            );
-          },
-        ),
-      ),
+      body: Container(
+          margin: EdgeInsets.symmetric(vertical: 50.0),
+          height: 400.0,
+          padding: EdgeInsets.only(left: 5.0),
+          child: ListView(
+            // This next line does the trick.
+            scrollDirection: Axis.horizontal,
+            children: <Widget>[
+              Container(
+                width: 350.0,
+                height: 400.0,
+                margin: EdgeInsets.symmetric(horizontal: 5.0),
+                color: Colors.red,
+                child: InkWell(
+                  onTap: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => Quiz()),
+                    );
+                  }, // handle your onTap here
+                  child: Container(height: 200, width: 200),
+                ),
+              ),
+              Container(
+                width: 350.0,
+                height: 400.0,
+                margin: EdgeInsets.symmetric(horizontal: 5.0),
+                color: Colors.blue,
+              ),
+              Container(
+                width: 350.0,
+                height: 400.0,
+                margin: EdgeInsets.symmetric(horizontal: 5.0),
+                color: Colors.green,
+              ),
+              Container(
+                width: 350.0,
+                height: 400.0,
+                margin: EdgeInsets.symmetric(horizontal: 5.0),
+                color: Colors.yellow,
+              ),
+              Container(
+                width: 350.0,
+                height: 400.0,
+                margin: EdgeInsets.symmetric(horizontal: 5.0),
+                color: Colors.orange,
+              ),
+            ],
+          )),
     );
   }
 }
